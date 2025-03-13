89a33b2555b038a1f6762d94f71f86f405c1dd00
Salma2032 <161228586+Salma2032@users.noreply.github.com>
2025-03-13 04:41:45 +0000

    Update reset.php

--- reset.php
+++ reset.php
@@ -78,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-    <link rel="icon" type="image/png" href="./images/fav.png">
+    <link rel="icon" type="image/png" href="./images/fav1.png">
     <style>
         body {
             font-family: Arial, sans-serif;
