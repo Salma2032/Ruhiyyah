2dca49b3f1f6cc72f57b5aa6afb3bab38efe4671
Salma2032 <161228586+Salma2032@users.noreply.github.com>
2025-03-13 12:58:37 +0000

    Update register.php

--- register.php
+++ register.php
@@ -135,7 +135,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>ConcentrEsi-Register</title>
+    <title>Ruhiyyah-Register</title>
     <link rel="icon" type="image/png" href="./images/fav1.png">
     <style>
         body {
