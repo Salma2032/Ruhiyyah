d8b2f78c3101f10f3672c1d764e88100ec0d3939
Salma2032 <161228586+Salma2032@users.noreply.github.com>
2025-03-13 12:55:40 +0000

    Update blog.php

--- blog.php
+++ blog.php
@@ -7,7 +7,7 @@ include 'session.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ConcentrEsi-Blog</title>
+    <title>Ruhiyyah-Blog</title>
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
