1b0c192e181e640c7312bc5822e34e8dd42613d1
Salma2032 <161228586+Salma2032@users.noreply.github.com>
2025-03-14 00:08:15 +0000

    Update blog.php

--- blog.php
+++ blog.php
@@ -7,7 +7,7 @@ include 'session.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ruhiyyah-Blog</title>
+    <title>Ruhiyyah-FAQ</title>
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -39,7 +39,7 @@ include 'session.php';
             </div>
             <i class="fa fa-bars" onclick="showMenu()"></i>
         </nav>
-        <h1>Blog</h1>
+        <h1>FAQ</h1>
     </header>   
 
     <!-- blog content -->
